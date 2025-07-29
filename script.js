37cf988183044885f59d862b6f983491a32e7561
Rulin <rulinchristians@gmail.com>
2025-07-29 21:14:13 +0200

    - Redesigned Testimonial section - Added functional carousal type to testimonial section

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ const observer = new IntersectionObserver(
 const cardElements = document.querySelectorAll(".card");
 cardElements.forEach((el) => observer.observe(el));
 // observer.observe(cardElements[0]);
+
+const TestimonialBtns = document.querySelectorAll(".testimonials__btn");
+const TestimonialCards = document.querySelectorAll(".testimonials__carousal");
+
+TestimonialBtns.forEach((btn, btnIndex) => {
+  btn.addEventListener("click", (clickedBtn) => {
+    TestimonialCards.forEach((card, cardIndex) => {
+      if (btnIndex === cardIndex) {
+        card.classList.remove("testimonial__hide");
+        TestimonialBtns.forEach((b) => {
+          b.classList.remove("active__btn");
+        });
+
+        clickedBtn.target.classList.add("active__btn");
+      } else {
+        card.classList.add("testimonial__hide");
+      }
+    });
+  });
+});
