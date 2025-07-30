b0ae5cfce4e45932f924c181a576d2c67f291919
Rulin <rulinchristians@gmail.com>
2025-07-30 19:43:08 +0200

    - Redesigned the connect banner section

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-/* Intersection Observer */
-const observer = new IntersectionObserver(
-  (entries) => {
-    /* This callback runs every-time an observer element enter/leaves the viewport */
+// /* Intersection Observer */
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     /* This callback runs every-time an observer element enter/leaves the viewport */
 
-    entries.forEach((entry) => {
-      /* To check if an entry is currently intersecting/visible */
-      if (entry.isIntersecting) {
-        console.log(entry);
-        entry.target.classList.add("ani");
-      } else {
-        entry.target.classList.remove("ani");
-      }
-    });
-  },
-  { threshold: 1 }
-);
+//     entries.forEach((entry) => {
+//       /* To check if an entry is currently intersecting/visible */
+//       if (entry.isIntersecting) {
+//         console.log(entry);
+//         entry.target.classList.add("ani");
+//       } else {
+//         entry.target.classList.remove("ani");
+//       }
+//     });
+//   },
+//   { threshold: 1 }
+// );
 
 /* For the observer to observe the .card class */
-const cardElements = document.querySelectorAll(".card");
-cardElements.forEach((el) => observer.observe(el));
+// const cardElements = document.querySelectorAll(".card");
+// cardElements.forEach((el) => observer.observe(el));
 // observer.observe(cardElements[0]);
 
 const TestimonialBtns = document.querySelectorAll(".testimonials__btn");
