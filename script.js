c51819e4eaed7623a0fa157214719dd1c9931a10
Rulin <rulinchristians@gmail.com>
2025-08-12 14:14:35 +0200

    - Minor name changes to content

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// /* Intersection Observer */
-// const observer = new IntersectionObserver(
-//   (entries) => {
-//     /* This callback runs every-time an observer element enter/leaves the viewport */
-
-//     entries.forEach((entry) => {
-//       /* To check if an entry is currently intersecting/visible */
-//       if (entry.isIntersecting) {
-//         console.log(entry);
-//         entry.target.classList.add("ani");
-//       } else {
-//         entry.target.classList.remove("ani");
-//       }
-//     });
-//   },
-//   { threshold: 1 }
-// );
-
-/* For the observer to observe the .card class */
-// const cardElements = document.querySelectorAll(".card");
-// cardElements.forEach((el) => observer.observe(el));
-// observer.observe(cardElements[0]);
-
 const TestimonialBtns = document.querySelectorAll(".testimonials__btn");
 const TestimonialCards = document.querySelectorAll(".testimonials__carousal");
 
